4ea94541c924c84189037392091344b14720d071
Adarsh Hiremath <ahiremath35@gmail.com>
2023-04-15 16:53:24 -0400

    Trying to implement with prompt engineering.

--- search.py
+++ search.py
@@ -1,37 +1,72 @@
 import json
-import numpy as np
+import sys
+import openai
+
+openai.api_key = "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"
 
 def load_classes(): 
     return json.load(open('courses_normalized.json'))
 
-def classes_json_to_numpy(classes_json):
-    classes_np = [["Id", "Subject", "Name", "Desc", "Profs", "Days", "StartTime", "EndTime", "HasSection"]]
-    
-    print(classes_json[39]["StartTime"])
+def clean_up(classes_json):
+    for i in range(len(classes_json)):
+        try: 
+            classes_json[i].pop("Subject")
+            classes_json[i].pop("Number")
+            classes_json[i].pop("ClassNumber")
+            classes_json[i].pop("CourseId")
+            classes_json[i].pop("Profs")
+            classes_json[i].pop("F22")
+            classes_json[i].pop("Days")
+            classes_json[i].pop("M")
+            classes_json[i].pop("T")
+            classes_json[i].pop("W")
+            classes_json[i].pop("R")
+            classes_json[i].pop("F")
+            classes_json[i].pop("StartTime")
+            classes_json[i].pop("EndTime")
+            classes_json[i].pop("HasSection")
+            classes_json[i].pop("AH")
+            classes_json[i].pop("Location")
+            classes_json[i].pop("URL")
+        except: 
+            continue
+    return classes_json
 
-    for i in range(8196):
+def search(query, classes_json):
+    documents = []
+    for class_info in classes_json:
         try:
-            id = classes_json[i]["Id"]
-            subject = classes_json[i]["Subject"]         
-            class_title = classes_json[i]["Name"]
-            class_desc = classes_json[i]["Desc"]
-            profs = classes_json[i]["Profs"]
-            days = classes_json[i]["Days"]
-            start_time = classes_json[i]["StartTime"]       
-            end_time = classes_json[i]["EndTime"]
-            has_section = classes_json[i]["HasSection"]
+            document = {
+                "id": class_info["id"],
+                "text": f"{class_info['name']} - {class_info['desc']}"
+            }
+            documents.append(document)
+        except KeyError as e:
+            print(f"Error processing class_info: {class_info}. Missing key: {e}")
 
-            classes_np.append([id, subject, class_title, class_desc, profs, days, start_time, end_time, has_section])
-        except KeyError:
-            pass
+    response = openai.Answer.create(
+        search_model="davinci",
+        model="davinci",
+        question=query,
+        documents=documents,
+        examples_context="",
+        max_responses=1,
+        lls_model="text-davinci-002",
+        return_prompt=True,
+        return_metadata=True,
+        stop=None,
+        temperature=0.5,
+    )
 
-    print (np.array(classes_np))
-        
+    most_relevant_id = response['choices'][0]['metadata']['id']
+
+    return most_relevant_id
+
 if __name__ == "__main__": 
     classes_json = load_classes()
-    classes_json_to_numpy(classes_json)
-    
+    search_query = input("Enter your search query: ")
+    result = search(search_query, classes_json)
+
+    print(f"The most relevant class ID for '{search_query}' is {result}.")
 
 
-
- 
