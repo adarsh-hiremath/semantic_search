709ab79e4c4fa5aaa4cd3a2a962e7a0546f478f9
Adarsh Hiremath <ahiremath35@gmail.com>
2023-04-17 11:58:57 -0400

    Update search.py
    Updating

--- search.py
+++ search.py
@@ -4,8 +4,16 @@ import numpy as np
 import pandas as pd
 import re
 from openai.embeddings_utils import cosine_similarity
+import transformers
+from transformers import AutoTokenizer, AutoModel
+import torch
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity as sklearn_cosine_similarity
+from sklearn.feature_extraction.text import strip_accents_unicode, strip_tags
+from sklearn.feature_extraction.text import _analyze
 
-openai.api_key = "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"
+
+openai.api_key = "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"
 
 
 def load_classes_and_embeddings():
@@ -33,7 +41,7 @@ def load_classes_and_embeddings():
         json.dump(courses, f)
 
 
-def calculate_similarity(query, query_embedding):
+def calculate_similarity_openai(query, query_embedding):
     """Calculates the cosine similarity between the query and the query embedding."""
 
     try:
@@ -48,6 +56,58 @@ def calculate_similarity(query, query_embedding):
         return -1
 
 
+def calculate_similarity_bert(query, query_embedding):
+    """Calculates the cosine similarity between the query and the query embedding."""
+    try:
+        query = np.array(query) if not isinstance(query, np.ndarray) else query
+        query_embedding = np.array(query_embedding) if not isinstance(
+            query_embedding, np.ndarray) else query_embedding
+
+        similarity = cosine_similarity(query, query_embedding)
+        if isinstance(similarity, np.ndarray):
+            return similarity[0]
+        else:
+            return similarity
+    except:
+        pass
+
+
+def bert_search(query, pprint=True, n=3):
+    """Searches the courses_embeddings.json file for the query and returns the top n results sorted by cosine similarity using BERT."""
+
+    df = pd.read_json('courses_embeddings.json')
+    df = df[:100]
+    query_embedding = encode_text(query)
+
+    df["similarity"] = df["embedding"].apply(
+        lambda x: calculate_similarity_bert(x, query_embedding))
+
+    result = df.sort_values("similarity", ascending=False).head(n)
+
+    if pprint:
+        for _, r in result.iterrows():
+            print("Course Name: " + re.sub(r'<\/?p>', '', str(r["Name"])))
+            print("Description: " + re.sub(r'<\/?p>', '', str(r["Desc"])))
+            print("Professors: " + re.sub(r'<\/?p>', '', str(r["Profs"])))
+            print("Start Time: " + re.sub(r'<\/?p>', '', str(r["StartTime"])))
+            print("End Time: " + re.sub(r'<\/?p>', '', str(r["EndTime"])))
+            print()
+
+    return df
+
+
+def encode_text(text):
+    """Encodes the text using BERT."""
+    model_name = "bert-base-uncased"
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model = AutoModel.from_pretrained(model_name)
+    tokens = tokenizer(text, padding=True, truncation=True,
+                       return_tensors="pt")
+    with torch.no_grad():
+        embeddings = model(**tokens).last_hidden_state[:, 0, :].numpy()
+    return embeddings[0]
+
+
 def openai_search(query, pprint=True, n=3):
     """Searches the courses_embeddings.json file for the query and returns the top n results sorted by cosine similarity."""
 
@@ -57,17 +117,17 @@ def openai_search(query, pprint=True, n=3):
         input=query, engine="text-embedding-ada-002")["data"][0]["embedding"]
 
     df["similarity"] = df["embedding"].apply(
-        lambda x: calculate_similarity(x, query_embedding))
+        lambda x: calculate_similarity_bert(x, query_embedding))
 
     result = df.sort_values("similarity", ascending=False).head(n)
 
     if pprint:
         for _, r in result.iterrows():
-            print("Course Name: " + re.sub(r'<\/?p>', '', r["Name"]))
-            print("Description: " + re.sub(r'<\/?p>', '', r["Desc"]))
-            print("Professors: " + re.sub(r'<\/?p>', '', r["Profs"]))
-            print("Start Time: " + re.sub(r'<\/?p>', '', r["StartTime"]))
-            print("End Time: " + re.sub(r'<\/?p>', '', r["EndTime"]))
+            print("Course Name: " + re.sub(r'<\/?p>', '', str(r["Name"])))
+            print("Description: " + re.sub(r'<\/?p>', '', str(r["Desc"])))
+            print("Professors: " + re.sub(r'<\/?p>', '', str(r["Profs"])))
+            print("Start Time: " + re.sub(r'<\/?p>', '', str(r["StartTime"])))
+            print("End Time: " + re.sub(r'<\/?p>', '', str(r["EndTime"])))
             print()
 
     return df
@@ -75,6 +135,19 @@ def openai_search(query, pprint=True, n=3):
 
 if __name__ == "__main__":
     # load_classes_and_embeddings()
-    query = input("Enter your search query: ")
     print()
-    result = openai_search(query)
+    type = input("Enter 1 for OpenAI and 2 for BERT: ")
+    print()
+    if (type == "1"):
+        query = input("Enter your search query: ")
+        print()
+        print("Results using OpenAI:")
+        print()
+        openai_result = openai_search(query)
+
+    if (type == "2"):
+        query = input("Enter your search query: ")
+        print()
+        print("Results using BERT:")
+        print()
+        bert_result = bert_search(query)
