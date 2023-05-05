04e08d60e54969ec88bead8ea301981105acc976
Adarsh Hiremath <ahiremath35@gmail.com>
2023-05-05 02:36:55 -0400

    Tf-idf

--- search.py
+++ search.py
@@ -11,10 +11,15 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity as sklearn_cosine_similarity
 from sklearn.feature_extraction.text import strip_accents_unicode, strip_tags
 from sklearn.feature_extraction.text import _analyze
-from fuzzywuzzy import fuzz
+from fuzzywuzzy import fuzz, process
+import logging
 
 
-openai.api_key = "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"
+openai.api_key = "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"
+logging.getLogger("transformers").setLevel(logging.CRITICAL)
+model_name = "bert-base-uncased"
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModel.from_pretrained(model_name)
 
 
 def load_classes_and_embeddings():
@@ -59,6 +64,7 @@ def calculate_similarity_openai(query, query_embedding):
 
 def calculate_similarity_bert(query, query_embedding):
     """Calculates the cosine similarity between the query and the query embedding."""
+
     try:
         query = np.array(query) if not isinstance(query, np.ndarray) else query
         query_embedding = np.array(query_embedding) if not isinstance(
@@ -77,7 +83,7 @@ def bert_search(query, pprint=True, n=3):
     """Searches the courses_embeddings.json file for the query and returns the top n results sorted by cosine similarity using BERT."""
 
     df = pd.read_json('courses_embeddings.json')
-    query_embedding = encode_text(query)
+    query_embedding = bert_encode(query)
 
     df["similarity"] = df["embedding"].apply(
         lambda x: calculate_similarity_bert(x, query_embedding))
@@ -93,11 +99,11 @@ def bert_search(query, pprint=True, n=3):
             print("End Time: " + re.sub(r'<\/?p>', '', str(r["EndTime"])))
             print()
 
-    return df
+    return result
 
 
 def fuzzy_search(query, pprint=True, n=3):
-    """Searches the courses_embeddings.json file with a fuzzy search and returns the top n results."""
+    """Searches the courses_combined.json file with a fuzzy search and returns the top n results."""
 
     df = pd.read_json('courses_combined.json')
 
@@ -115,12 +121,49 @@ def fuzzy_search(query, pprint=True, n=3):
             print("End Time: " + re.sub(r'<\/?p>', '', str(r["EndTime"])))
             print()
 
+    return result
 
-def encode_text(text):
+
+def tfidf_search(query, pprint=True, n=3):
+    """Searches the courses_normalized.json file for the query and returns the top n results sorted by cosine similarity using tf-idf."""
+
+    df = pd.read_json('courses_embeddings.json')
+    df = df.dropna()
+
+    corpus = df['combined']
+
+    vectorizer = TfidfVectorizer(
+        analyzer='word', ngram_range=(1, 3), min_df=0.2)
+
+    tfidf_matrix = vectorizer.fit_transform(corpus)
+
+    query_matrix = vectorizer.transform([query])
+
+    result_indices = []
+    try:
+        cosine_similarities = sklearn_cosine_similarity(
+            query_matrix, tfidf_matrix).flatten()
+        result_indices = cosine_similarities.argsort()[::-1][:n]
+    except:
+        print("No courses found matching your query.")
+        return
+
+    result = courses.iloc[result_indices]  # get the results using the indices
+    if pprint:
+        for _, r in result.iterrows():
+            print("Course Name: " + re.sub(r'<\/?p>', '', str(r["Name"])))
+            print("Description: " + re.sub(r'<\/?p>', '', str(r["Desc"])))
+            print("Professors: " + re.sub(r'<\/?p>', '', str(r["Profs"])))
+            print("Start Time: " + re.sub(r'<\/?p>', '', str(r["StartTime"])))
+            print("End Time: " + re.sub(r'<\/?p>', '', str(r["EndTime"])))
+            print()
+
+    return result
+
+
+def bert_encode(text):
     """Encodes the text using BERT."""
-    model_name = "bert-base-uncased"
-    tokenizer = AutoTokenizer.from_pretrained(model_name)
-    model = AutoModel.from_pretrained(model_name)
+
     tokens = tokenizer(text, padding=True, truncation=True,
                        return_tensors="pt")
     with torch.no_grad():
@@ -177,9 +220,16 @@ if __name__ == "__main__":
         elif (search_type == "3"):
             query = input("Enter your search query: ")
             print()
-            print("Results using fuzzy matching library:")
+            print("Results using fuzzy matching:")
             print()
             fuzzy_result = fuzzy_search(query)
+
+        elif (search_type == "4"):
+            query = input("Enter your search query: ")
+            print()
+            print("Results using TF-IDF retrieval:")
+            print()
+            tfidf_result = tfidf_search(query)
 
         else:
             print()
